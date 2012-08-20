d324ead38260f1f41d47c5a66c140f0665492ea0
root <root@h1781126.stratoserver.net>
2012-08-20 17:24:34 +0200

    setup correct manifest for installation

--- appmanifest.php
+++ appmanifest.php
@@ -2,19 +2,17 @@
 header('Content-type: application/x-web-app-manifest+json');
 ?>{
   "version": "1.0",
-  "name": "FirefoxOS App",
+  "name": "Martin's FirefoxOS App",
+  "launch_path": "/b2gapp/",
   "description": "A demo application to show how a FirefoxOS/B2G app can be distributed!",
   "icons": {
-    "16": "/img/icon-16.png",
-    "48": "/img/icon-48.png",
-    "128": "/img/icon-128.png"
+    "128": "/b2gapp/img/icon-128.png"
   },
   "developer": {
     "name": "Martin de Keijzer",
     "url": "http://www.martindekeijzer.nl"
   },
   "installs_allowed_from": ["*"],
-  "appcache_path": "/cache.manifest",
   "orientation": "portrait,landscape-secondary",
   "fullscreen": "true"
 }
